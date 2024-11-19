70ce0a4230dde301a91d30ed10581a283921155d
yossisabato <yossi.sabto@gmail.com>
2024-11-19 14:05:48 +0200

    Update header.php

--- header.php
+++ header.php
@@ -10,6 +10,12 @@
 <header>
     <h1><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
     <nav>
-        <?php wp_nav_menu(array('theme_location' => 'main-menu')); ?>
+        <?php
+        wp_nav_menu( array(
+            'theme_location' => 'main-menu',
+            'container' => false,
+            'menu_class' => 'main-menu'
+        ) );
+        ?>
     </nav>
 </header>
