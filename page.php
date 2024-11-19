3c249d997ca7f03e409b4292163ca8b1c282d3fa
yossisabato <yossi.sabto@gmail.com>
2024-11-19 13:56:18 +0200

    Create page.php
    This template will handle standard pages

---
+++ page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<main>
+    <?php
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+            the_content();
+        endwhile;
+    else :
+        echo '<p>No content found</p>';
+    endif;
+    ?>
+</main>
+<?php get_footer(); ?>
