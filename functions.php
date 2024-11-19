ab7f18e1eaa7ca567703c176037d92110e17708b
yossisabato <yossi.sabto@gmail.com>
2024-11-19 13:49:35 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+function moran_sabato_setup() {
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'custom-logo' );
+    add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+}
+add_action( 'after_setup_theme', 'moran_sabato_setup' );
+
+function moran_sabato_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'moran_sabato_scripts' );
