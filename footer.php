79828f53c666344569375759cc24bdcf272dc427
yossisabato <yossi.sabto@gmail.com>
2024-11-24 18:22:43 +0200

    Update footer.php
    adding whatsapp

--- footer.php
+++ footer.php
@@ -2,5 +2,10 @@
     <p>&copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
 </footer>
 <?php wp_footer(); ?>
+<body>
+    <!-- Add WhatsApp button here -->
+    <a href="https://wa.me/0528751769" class="whatsapp-floating-button" target="_blank" rel="noopener">
+        <img src="http://moransabato.local/wp-content/uploads/2024/11/whatsapp.png" alt="WhatsApp Chat">
+    </a>
 </body>
 </html>
