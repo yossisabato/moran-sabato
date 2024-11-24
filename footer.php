a468adc590b2a9e6f836d349e33ea4aa234f6290
yossisabato <yossi.sabto@gmail.com>
2024-11-24 19:41:45 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <?php wp_footer(); ?>
 <body>
     <!-- Add WhatsApp button here -->
-    <a href="https://wa.me/+972528751769?text=היי%20אשמח%20שתצרי@20איתי%20קשר" class="whatsapp-floating-button" target="_blank" rel="noopener">
+    <a href="https://wa.me/+972528751769?text=שלום,%20אשמח%20שתצרי%20איתי%20קשר" class="whatsapp-floating-button" target="_blank" rel="noopener">
         <img src="http://moransabato.local/wp-content/uploads/2024/11/whatsapp-icon.jpeg" alt="WhatsApp Chat">
     </a>
 </body>
