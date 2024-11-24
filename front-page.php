bd124f4f853956eee5746cffa7e13faaf6949e2d
yossisabato <yossi.sabto@gmail.com>
2024-11-24 14:30:37 +0200

    Update front-page.php
    logo

--- front-page.php
+++ front-page.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
 <main>
     <section>
+        <div class="site-logo">
+        <img src="<?php echo esc_url('http://moransabato.local/wp-content/uploads/2024/11/moran-logo.pdf'); ?>" alt="מרכז טיפולי מורן סבתו">
+    </div>
         <h1>מרכז טיפולי - מורן סבתו</h1>
         <p>מרכז מהתמחה בטיפול מיני, זוגי, משפחתי, טיפול בטראומה, וטיפול בפגיעות מיניות - פוגעים ונפגעים</p>
     </section>
