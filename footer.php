9e360bc650eb813791da0bb6a3173bbb1ba3d7ef
yossisabato <yossi.sabto@gmail.com>
2024-11-24 18:24:53 +0200

    Update footer.php
    icon

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 <body>
     <!-- Add WhatsApp button here -->
     <a href="https://wa.me/0528751769" class="whatsapp-floating-button" target="_blank" rel="noopener">
-        <img src="http://moransabato.local/wp-content/uploads/2024/11/whatsapp.png" alt="WhatsApp Chat">
+        <img src="http://moransabato.local/wp-content/uploads/2024/11/whatsapp-icon.jpeg" alt="WhatsApp Chat">
     </a>
 </body>
 </html>
