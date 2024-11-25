7fdcecb9eae27e861bfc73ca09b6f80ea2210019
yossisabato <yossi.sabto@gmail.com>
2024-11-25 18:28:46 +0200

    Create popup.js

---
+++ js/popup.js
@@ -0,0 +1,20 @@
+// Open the popup
+function openPopup(imgSrc) {
+    const popup = document.getElementById("image-popup");
+    const popupImg = document.getElementById("popup-img");
+    popup.style.display = "flex";
+    popupImg.src = imgSrc;
+}
+
+// Close the popup
+function closePopup() {
+    const popup = document.getElementById("image-popup");
+    popup.style.display = "none";
+}
+
+// Add click event to all images
+document.querySelectorAll(".popup-image").forEach((image) => {
+    image.addEventListener("click", function () {
+        openPopup(this.src);
+    });
+});
