82014d8f25b502f41559b42ab30fea7e97877832
yossisabato <yossi.sabto@gmail.com>
2024-11-25 16:20:59 +0200

    Update page-contact.php

--- page-contact.php
+++ page-contact.php
@@ -8,8 +8,8 @@ get_header();
 <div class="container">
     <!-- Page Header -->
     <div class="page-header">
-        <img src="<?php echo get_template_directory_uri(); ?>http://moransabato.local/wp-content/uploads/2024/11/moran-logo.png" alt="Logo">
-        <img src="<?php echo get_template_directory_uri(); ?>http://moransabato.local/wp-content/uploads/2024/11/Moran-scaled.jpg" alt="Moran Sabato">
+        <img src="<?php echo esc_url('http://moransabato.local/wp-content/uploads/2024/11/moran-logo.png'); ?>" alt="מרכז טיפולי">
+        <img src="<?php echo esc_url('http://moransabato.local/wp-content/uploads/2024/11/Moran-scaled.jpg'); ?>" alt="מורן סבתו">
     </div>
 
     <!-- Static Information -->
