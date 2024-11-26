32d2781c551030fec133178d354506e46524a9ed
yossisabato <yossi.sabto@gmail.com>
2024-11-26 13:49:33 +0200

    Update page-contact.php

--- page-contact.php
+++ page-contact.php
@@ -31,7 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Attempt to send email
     if (wp_mail($to, $email_subject, $email_body, $headers)) {
         $_SESSION['success_message'] = "תודה שפנית למורן! אצור איתך קשר בהקדם.";
-        wp_safe_redirect(home_url('/')); // Redirect after successful submission
+        wp_redirect(home_url('/contact/'));
         exit;
     } else {
         $_SESSION['error_message'] = "שליחת הטופס נכשלה. אנא נסה שוב מאוחר יותר, ניתן לשלוח ווטסאפ ל 0528-751769.";
@@ -62,15 +62,11 @@ if (!empty($_SESSION['error_message'])) {
         </div>
         <img src="http://moransabato.local/wp-content/uploads/2024/11/moran-logo.png" alt="Logo" class="contact-logo" style="width: 200px; border-radius: 10px;">
     </div>
-
-    <!-- Display Messages -->
-    <?php if ($success_message): ?>
-        <div class="success-message"><?php echo $success_message; ?></div>
-    <?php elseif ($error_message): ?>
-        <div class="error-message"><?php echo $error_message; ?></div>
-    <?php endif; ?>
+    
     <!-- Contact Form -->
-    <form action="" method="POST">
+<form action="<?php echo esc_url($_SERVER['REQUEST_URI']); ?>" method="POST">
+
+<!--    <form action="" method="POST"> -->
         <label for="name">שם *:</label>
         <input type="text" id="name" name="name" required>
 
@@ -99,6 +95,14 @@ if (!empty($_SESSION['error_message'])) {
         <button type="submit" class="submit-button">שלח</button>
     </form>
 </div>
+
+<!-- Display Messages -->
+<?php if (isset($_SESSION['success_message'])): ?>
+    <div class="success-message">
+        <?php echo $_SESSION['success_message']; ?>
+        <?php unset($_SESSION['success_message']); ?>
+    </div>
+<?php endif; ?>
 
 <!-- Optional: Remove this if you don't want alerts -->
 <script>
