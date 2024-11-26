3c7b426c64f76785200e4b422cb934e1bd9f378e
yossisabato <yossi.sabto@gmail.com>
2024-11-26 16:24:04 +0200

    Update front-page.php
    Improve the Styling of Colors and Font

--- front-page.php
+++ front-page.php
@@ -21,7 +21,41 @@
         שירות מקצועי ויחס אישי שיעזור לכם לשפר את חייכם.
     </p>
 </section>
-
+    
+<section class="service-section">
+    <img src="http://moransabato.local/wp-content/uploads/2024/11/couple-bg-1-1.jpg" alt="Service Image" class="service-image">
+    <div class="service-content">
+        <h2 class="service-title">טיפול זוגי</h2>
+        <p class="service-description">טיפול זוגי מסייע לשיפור הקשר הזוגי והחיבור בין בני הזוג.</p>
+        <a href="<?php echo site_url('/service/couples-therapy'); ?>" class="read-more-button">קרא עוד</a>
+    </div>
+</section>
+    
+    <section class="service-section">
+    <img src="http://moransabato.local/wp-content/uploads/2024/11/lisa-yee-therapy-core-services-intimate-connection-building-icon.png" alt="Service Image" class="service-image">
+    <div class="service-content">
+        <h2 class="service-title">טיפול מיני</h2>
+        <p class="service-description">טיפול מיני בזוגות ויחידים במטרה לשפר את חיי המין./p>
+        <a href="<?php echo site_url('/service/couples-therapy'); ?>" class="read-more-button">קרא עוד</a>
+    </div>
+</section>
+    <section class="service-section">
+    <img src="http://moransabato.local/wp-content/uploads/2024/11/trauma.jpg" alt="Service Image" class="service-image">
+    <div class="service-content">
+        <h2 class="service-title">טיפול בטראומה</h2>
+        <p class="service-description">טיפול בטראומה על מנת לעזור בהתמודדיות עם טראומות ולשיפור איכות החיים</p>
+        <a href="<?php echo site_url('/service/couples-therapy'); ?>" class="read-more-button">קרא עוד</a>
+    </div>
+</section>
+    <section class="service-section">
+    <img src="http://moransabato.local/wp-content/uploads/2024/11/png-clipart-genealogy-family-constellations-family-therapy-psychogenealogie-family-people-cartoon-thumbnail.png" alt="Service Image" class="service-image">
+    <div class="service-content">
+        <h2 class="service-title">טיפול משפחתי</h2>
+        <p class="service-description">טיפול משפחתי מסייע לשיפור הקשר המשפחתי והחיבורים בתוך המשפחה.</p>
+        <a href="<?php echo site_url('/service/couples-therapy'); ?>" class="read-more-button">קרא עוד</a>
+    </div>
+</section>
+<!--
     <div class="service">
     <div class="image-section" style="background-image: url('http://moransabato.local/wp-content/uploads/2024/11/couple-bg-1-1.jpg');"></div>
     <div class="text-section">
@@ -56,7 +90,7 @@
         </div>
         </div>
     </section>
-
+-->
     <!-- Call-to-Action Section -->
     <section class="cta">
         <h2>צרו קשר עוד היום</h2>
