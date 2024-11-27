5f6cc78093d4feefc9f57dfa5b7fcdd71e9e2338
yossisabato <yossi.sabto@gmail.com>
2024-11-27 16:02:39 +0200

    Update footer-home.php

--- footer-home.php
+++ footer-home.php
@@ -1,1 +1,11 @@
-
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> מורן סבתו כל הזכויות שמורות 0528-751769 </p>
+</footer>
+<?php wp_footer(); ?>
+<body>
+    <!-- Add WhatsApp button here -->
+    <a href="https://wa.me/+972528751769?text=שלום,%20אשמח%20שתצרי%20איתי%20קשר" class="whatsapp-floating-button" target="_blank" rel="noopener">
+        <img src="http://moransabato.local/wp-content/uploads/2024/11/whatsapp-icon.jpeg" alt="WhatsApp Chat">
+    </a>
+</body>
+</html>
