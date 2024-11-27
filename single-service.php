4e1d344e2b927ecb4b6fb1c90b5cf6059d0756c7
yossisabato <yossi.sabto@gmail.com>
2024-11-27 15:52:45 +0200

    Update single-service.php

--- single-service.php
+++ single-service.php
@@ -18,6 +18,13 @@
     <div class="contact-section">
         <?php get_template_part('template-parts/contact-form'); ?>
     </div>
+
+    <!-- Call-to-Action Section -->
+    <section class="cta">
+        <h2> יצירת קשר  </h2>
+        <p>מתלבטים? צוות המטפלים שלנו כאן בשבילכם. צרו קשר עוד היום ונחזור אליכם בהקדם.</p>
+        <a href="<?php echo site_url('/contact'); ?>" class="cta-button">צור קשר</a>
+    </section>
     
     <!-- WhatsApp Icon -->
     <a href="https://wa.me/972528751769" class="whatsapp-icon" target="_blank">
