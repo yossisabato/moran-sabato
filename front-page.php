67398717345bf6b428bb37c1d5bf2974dd6f59d7
yossisabato <yossi.sabto@gmail.com>
2024-11-27 15:49:32 +0200

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -67,42 +67,6 @@
         <a href="<?php echo site_url('/service/family-therapy'); ?>" class="read-more-button">קרא עוד</a>
     </div>
 </section>
-<!--
-    <div class="service">
-    <div class="image-section" style="background-image: url('http://moransabato.local/wp-content/uploads/2024/11/couple-bg-1-1.jpg');"></div>
-    <div class="text-section">
-        <h3>טיפול זוגי</h3>
-        <p>טיפול זוגי מסייע לשיפור הקשר הזוגי והחיבור בין בני הזוג.</p>
-        <a href="#">קרא עוד</a>
-    </div>
-    </div>
-
-        <div class="service">
-            <div class="image-section" style="background-image: url('http://moransabato.local/wp-content/uploads/2024/11/png-clipart-genealogy-family-constellations-family-therapy-psychogenealogie-family-people-cartoon-thumbnail.png');"></div>
-            <div class="text-section">
-            <h3>טיפול משפחתי</h3>
-            <p>טיפול משפחתי מסייע לשיפור הקשר המשפחתי והחיבורים בתוך המשפחה.</p>
-            <a href="#">קרא עוד</a>
-        </div>
-        </div>
-        <div class="service">
-            <div class="image-section" style="background-image: url('http://moransabato.local/wp-content/uploads/2024/11/trauma.jpg');"></div>
-            <div class="text-section">
-            <h3>טיפול בטראומה</h3>
-            <p>עוזר לאנשים להתמודד עם טראומות ולשפר את איכות חייהם.</p>
-            <a href="#">קרא עוד</a>
-        </div>
-        </div>
-        <div class="service">
-            <div class="image-section" style="background-image: url('http://moransabato.local/wp-content/uploads/2024/11/lisa-yee-therapy-core-services-intimate-connection-building-icon.png');"></div>
-            <div class="text-section">
-            <h3>טיפול מיני</h3>
-            <p>טיפול מיני תומך בזוגות ויחידים במטרה לשפר את חיי המין.</p>
-            <a href="#">קרא עוד</a>
-        </div>
-        </div>
-    </section>
--->
     <!-- Call-to-Action Section -->
     <section class="cta">
         <h2>צרו קשר עוד היום</h2>
